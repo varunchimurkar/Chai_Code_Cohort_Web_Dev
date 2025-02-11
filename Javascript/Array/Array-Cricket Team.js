9ada1c2def09be13986c74d2ff0a5af154af5792
varunchimurkar <varunchimurkar96@gmail.com>
2025-02-11 16:51:54 +0530

    change name Array to Array-Cricket Team

---
+++ Javascript/Array/Array-Cricket Team.js
@@ -0,0 +1,11 @@
+//Array.at() Method :- The at() method of Array instances takes an integer value and returns the item at that index,
+//allowing for positive and negative integers. Negative integers count back from the last item in the array.
+
+
+const array1 = []
+
+console.log(array1.at(6));
+
+
+
+
--- Javascript/Array/array.js
+++
@@ -1,9 +0,0 @@
-//Array.at() Method
-const array1 = [1,5,44,55,33,565,34,56]
-
-let index = 6;
-
-console.log()
-
-
-
